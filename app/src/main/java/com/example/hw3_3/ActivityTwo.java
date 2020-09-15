5da418bd491a07c7bd22aad0ea2c4bf11a6b129f
hadjiev0111 <amirhadjiev18@gmail.com>
2020-09-15 20:20:41 +0600

    SalamAleykum

--- app/src/main/java/com/example/hw3_3/ActivityTwo.java
+++ app/src/main/java/com/example/hw3_3/ActivityTwo.java
@@ -2,23 +2,33 @@ package com.example.hw3_3;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class ActivityTwo extends AppCompatActivity {
+
     private ImageView imgChoose;
     private EditText etTxt;
     private Button btnSend;
-    private Uri imageData;
-    public static String KEY = "key";
-    public static int REQUEST_CODE = 100;
+    private final int RESULT_LOAD_IMG = 10;
+    public static final int REQUEST_CODE = 100;
+
+    protected String textImage;
+    protected Uri uri;
+    protected int PICK_IMAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,37 +44,31 @@ public class ActivityTwo extends AppCompatActivity {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendData();
+                Intent intentData = new Intent();
+                intentData.putExtra(MainActivity.TEXT_RESULT, etTxt.getText().toString().trim());
+                intentData.putExtra("image", textImage);
+                setResult(RESULT_OK, intentData);
+                finish();
             }
         });
         imgChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chooseImg(view);
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
             }
         });
-    }
-
-    private void sendData() {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("info", etTxt.getText().toString());
-        setResult(RESULT_OK, intent);
-        finish();
-    }
-
-    private void chooseImg(View view) {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser (intent, "choose img"), 200);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 200 && resultCode == RESULT_OK);
-        imageData = data.getData();
-        imgChoose.setImageURI(imageData);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE && data != null) {
+            uri = data.getData();
+            textImage = data.getDataString();
+            imgChoose.setImageURI(uri);
+        }
     }
 }
--- app/src/main/java/com/example/hw3_3/MainActivity.java
+++ app/src/main/java/com/example/hw3_3/MainActivity.java
@@ -1,79 +1,68 @@
 package com.example.hw3_3;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static com.example.hw3_3.ActivityTwo.REQUEST_CODE;
 
 public class MainActivity extends AppCompatActivity {
+    protected ImageView imageView;
     private TextView textResult;
     private Button btnOpen, btnEmail;
+    public static final String TEXT_RESULT = "textResult";
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        imageView = findViewById(R.id.imageView);
         textResult = findViewById(R.id.getTxt);
         btnOpen = findViewById(R.id.btnOpen);
+        btnEmail = findViewById(R.id.btnEmail);
         btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                open();
+                startActivityForResult(new Intent(MainActivity.this,ActivityTwo.class),REQUEST_CODE);
             }
         });
-        btnEmail = findViewById(R.id.btnEmail);
+
         btnEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendEmail();
+                Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(android.content.Intent.EXTRA_TEXT,textResult.getText().toString().trim());
+                startActivity(intent);
             }
         });
-    }
-
-    private void open() {
-        Intent intent = new Intent(this,ActivityTwo.class);
-        startActivityForResult(intent, ActivityTwo.REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 100 && resultCode == RESULT_OK);
-        String a = data.getStringExtra("info");
-        textResult.setText(a);
-    }
-
-    @SuppressLint("LongLogTag")
-    private void sendEmail() {
-        Log.i("Send email", "");
-        String[] TO = {""};
-        String[] CC = {""};
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-
-        emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.setType("text/plain");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-        emailIntent.putExtra(Intent.EXTRA_CC, CC);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
-
-        try {
-            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-            finish();
-            Log.i("Finished sending email...", "");
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE && data!=null) {
+            String text = data.getStringExtra(TEXT_RESULT);
+            Uri uri = Uri.parse(data.getStringExtra("image"));
+            textResult.setText(text);
+            imageView.setImageURI(uri);
         }
+
+
+
     }
 }
